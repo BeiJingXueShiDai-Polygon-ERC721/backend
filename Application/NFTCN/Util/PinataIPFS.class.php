5281bb2de46072f214c0498bd3ee87372d15bab1
fantianmi@163.com <fantianmi@163.com>
2022-06-27 22:16:49 +0800

    s

--- Application/NFTCN/Util/PinataIPFS.class.php
+++ Application/NFTCN/Util/PinataIPFS.class.php
@@ -22,11 +22,13 @@ class PinataIPFS
 
     public function pinFileToIPFS($file, $name)
     {
+        $fileFullPath = $file['file']['tmp_name'] ."/". $file['file']['name'];
         $post = [
             'pinataOptions' => '{\"cidVersion\": 1}',
             'pinataMetadata' => "{\"name\": \"{$name}\", \"keyvalues\": {\"company\": \"Pinata\"}}"
         ];
         var_dump($file);
+        var_dump($fileFullPath);
         var_dump($post);
     }
 
